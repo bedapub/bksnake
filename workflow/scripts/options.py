3c564d330d531e4fdbfba6419d00e9f3a5b63e1b
Schmucki <roland.schmucki@roche.com>
2024-03-19 00:55:45 -0700

    v8

--- workflow/scripts/options.py
+++ workflow/scripts/options.py
@@ -22,53 +22,46 @@ def get_optional_output_files(sample_ids, config):
     optional_output_files = []
     if config['keep_fastq_files'] == True:
         if config['library']['type'] == 'paired-end':
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}_1.fastq.gz'), sample=sample_ids))
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}_2.fastq.gz'), sample=sample_ids))
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}_1.fastq.gz'), sample=sample_ids)
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}_2.fastq.gz'), sample=sample_ids)
         else:
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}.fastq.gz'), sample=sample_ids))
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}.fastq.gz'), sample=sample_ids)
     else:
         if config['library']['type'] == 'paired-end':
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}_1.fastq.gz_to_delete'), sample=sample_ids))
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}_2.fastq.gz_to_delete'), sample=sample_ids))
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}_1.fastq.gz_to_delete'), sample=sample_ids)
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}_2.fastq.gz_to_delete'), sample=sample_ids)
         else:
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}.fastq.gz_to_delete'), sample=sample_ids))
-
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}.fastq.gz_to_delete'), sample=sample_ids)
 
     if config['keep_bam_files'] == True:
-        optional_output_files.append(expand(os.path.join(OD_BAM, '{sample}.bam'), sample=sample_ids))
-        optional_output_files.append(expand(os.path.join(OD_BAM, '{sample}.bam.bai'), sample=sample_ids))
+        optional_output_files += expand(os.path.join(OD_BAM, '{sample}.bam'), sample=sample_ids)
+        optional_output_files += expand(os.path.join(OD_BAM, '{sample}.bam.bai'), sample=sample_ids)
     else:
-        optional_output_files.append(expand(os.path.join(OD_BAM, '{sample}.bam_to_delete'), sample=sample_ids))
-        optional_output_files.append(expand(os.path.join(OD_BAM, '{sample}.bam.bai_to_delete'), sample=sample_ids))
-
+        optional_output_files += expand(os.path.join(OD_BAM, '{sample}.bam_to_delete'), sample=sample_ids)
+        optional_output_files += expand(os.path.join(OD_BAM, '{sample}.bam.bai_to_delete'), sample=sample_ids)
 
     if config['generate_cram_files'] == True:
-        optional_output_files.append(expand(os.path.join(OD_CRAM, '{sample}.cram'), sample=sample_ids))
-        optional_output_files.append(expand(os.path.join(OD_CRAM, '{sample}.cram.crai'), sample=sample_ids))
-
+        optional_output_files += expand(os.path.join(OD_CRAM, '{sample}.cram'), sample=sample_ids)
+        optional_output_files += expand(os.path.join(OD_CRAM, '{sample}.cram.crai'), sample=sample_ids)
 
     if config['generate_bw_files'] == True:
-        optional_output_files.append(expand(os.path.join(OD_BW, '{sample}.bw'), sample=sample_ids))
-
+        optional_output_files += expand(os.path.join(OD_BW, '{sample}.bw'), sample=sample_ids)
 
     if config['generate_unmapped'] == True:
         if config['library']['type'] == 'paired-end':
-            optional_output_files.append(expand(os.path.join(OD, 'unmapped', '{sample}_1.fastq.gz'), sample=sample_ids))
-            optional_output_files.append(expand(os.path.join(OD, 'unmapped', '{sample}_2.fastq.gz'), sample=sample_ids))
+            optional_output_files += expand(os.path.join(OD, 'unmapped', '{sample}_1.fastq.gz'), sample=sample_ids)
+            optional_output_files += expand(os.path.join(OD, 'unmapped', '{sample}_2.fastq.gz'), sample=sample_ids)
         else:
-            optional_output_files.append(expand(os.path.join(OD, 'unmapped', '{sample}_1.fastq.gz'), sample=sample_ids))
-
+            optional_output_files += expand(os.path.join(OD, 'unmapped', '{sample}_1.fastq.gz'), sample=sample_ids)
 
     if config['organism'] == 'Homo sapiens' and 'crosscheck_fingerprints' in config:
         if config['crosscheck_fingerprints'] == True:
-            optional_output_files.append(os.path.join(OD_METRICS, 'crosscheck_metrics'))
+            optional_output_files += [os.path.join(OD_METRICS, 'crosscheck_metrics')]
 
-
+    #print('List of all optional output files')
+    #from pprint import pprint
+    #pprint(optional_output_files)
     return optional_output_files
-#print('List of all optional output files')
-#from pprint import pprint
-#pprint(optional_output_files)
-
 
 
 # ------------------------------------------------------------------------------
@@ -85,43 +78,36 @@ def get_optional_output_files_vc(sample_ids, config):
     optional_output_files = []
     if config['keep_fastq_files'] == True:
         if config['library']['type'] == 'paired-end':
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}_1.fastq.gz'), sample=sample_ids))
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}_2.fastq.gz'), sample=sample_ids))
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}_1.fastq.gz'), sample=sample_ids)
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}_2.fastq.gz'), sample=sample_ids)
         else:
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}.fastq.gz'), sample=sample_ids))
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}.fastq.gz'), sample=sample_ids)
     else:
         if config['library']['type'] == 'paired-end':
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}_1.fastq.gz_to_delete'), sample=sample_ids))
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}_2.fastq.gz_to_delete'), sample=sample_ids))
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}_1.fastq.gz_to_delete'), sample=sample_ids)
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}_2.fastq.gz_to_delete'), sample=sample_ids)
         else:
-            optional_output_files.append(expand(os.path.join(OD_FASTQ, '{sample}.fastq.gz_to_delete'), sample=sample_ids))
-
+            optional_output_files += expand(os.path.join(OD_FASTQ, '{sample}.fastq.gz_to_delete'), sample=sample_ids)
 
     if config['keep_bam_files'] == True:
-        optional_output_files.append(expand(os.path.join(OD_BAM, '{sample}.bam'), sample=sample_ids))
-        optional_output_files.append(expand(os.path.join(OD_BAM, '{sample}.bam.bai'), sample=sample_ids))
+        optional_output_files += expand(os.path.join(OD_BAM, '{sample}.bam'), sample=sample_ids)
+        optional_output_files += expand(os.path.join(OD_BAM, '{sample}.bam.bai'), sample=sample_ids)
     else:
-        optional_output_files.append(expand(os.path.join(OD_BAM, '{sample}.bam_to_delete'), sample=sample_ids))
-        optional_output_files.append(expand(os.path.join(OD_BAM, '{sample}.bam.bai_to_delete'), sample=sample_ids))
-
+        optional_output_files += expand(os.path.join(OD_BAM, '{sample}.bam_to_delete'), sample=sample_ids)
+        optional_output_files += expand(os.path.join(OD_BAM, '{sample}.bam.bai_to_delete'), sample=sample_ids)
 
     if config['generate_cram_files'] == True:
-        optional_output_files.append(expand(os.path.join(OD_CRAM, '{sample}.cram'), sample=sample_ids))
-        optional_output_files.append(expand(os.path.join(OD_CRAM, '{sample}.cram.crai'), sample=sample_ids))
-
+        optional_output_files += expand(os.path.join(OD_CRAM, '{sample}.cram'), sample=sample_ids)
+        optional_output_files += expand(os.path.join(OD_CRAM, '{sample}.cram.crai'), sample=sample_ids)
 
     #if config['generate_bw_files'] == True:
-    #    optional_output_files.append(expand(os.path.join(OD_BW, '{sample}.bw'), sample=sample_ids))
-
+    #    optional_output_files += expand(os.path.join(OD_BW, '{sample}.bw'), sample=sample_ids)
 
     #if config['generate_unmapped'] == True:
     #    if config['library']['type'] == 'paired-end':
-    #        optional_output_files.append(expand(os.path.join(OD, 'unmapped', '{sample}_1.fastq.gz'), sample=sample_ids))
-    #        optional_output_files.append(expand(os.path.join(OD, 'unmapped', '{sample}_2.fastq.gz'), sample=sample_ids))
+    #        optional_output_files += expand(os.path.join(OD, 'unmapped', '{sample}_1.fastq.gz'), sample=sample_ids)
+    #        optional_output_files += expand(os.path.join(OD, 'unmapped', '{sample}_2.fastq.gz'), sample=sample_ids)
     #    else:
-    #        optional_output_files.append(expand(os.path.join(OD, 'unmapped', '{sample}_1.fastq.gz'), sample=sample_ids))
+    #        optional_output_files += expand(os.path.join(OD, 'unmapped', '{sample}_1.fastq.gz'), sample=sample_ids)
 
     return optional_output_files
-#print('List of all optional output files')
-#from pprint import pprint
-#pprint(optional_output_files)
